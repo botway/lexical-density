32887913eb7e29ea44f1d30cf00901ee5b623321
Andrei Pilui <zikorij@yahoo.de>
2019-04-12 10:52:16 +0200

    retrieving data from mongodb

--- db.js
+++ db.js
@@ -1,21 +1,36 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/';
+const url = 'mongodb://localhost:27017/lingDB';
 const fs = require('fs');
 
-module.exports = MongoClient.connect(url, function(err, db) {
-    // const collection = db.collection('nonlexWords')
-    //get nonlexical words from a txt file
-    if (err) throw err;
-    const dbo = db.db("nonlexDB");
-    console.log("db", db);
+exports.connect = function() {
+    return new Promise(function(resolve, reject) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            //if collection doesn't exist create one and add data
+            const dbo = db.db("lingDB")
 
-    require.extensions['.txt'] = function (module, filename) {
-        module.exports = fs.readFileSync(filename, 'utf8');
-    };
-    const nonlexStr = require("./nonlex.txt");
-    let nonlex = nonlexStr.split("\n").map(el => el.slice(0,-1));
-    dbo.collection("nonlexWords").insertOne({nonlex: nonlex}, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-    })
-});
+            dbo.listCollections({
+                name: "nonlexWords"
+            }).toArray((err, items) => {
+                // console.log(items.length, items);
+                let data;
+                if (items.length == 0) {
+                    require.extensions['.txt'] = function(module, filename) {
+                        module.exports = fs.readFileSync(filename, 'utf8');
+                    };
+                    const nonlexStr = require("./nonlex.txt");
+                    let nonlex = nonlexStr.split("\n").map(el => el.slice(0, -1));
+                    nonlex.pop(); //removing an empty index from txt file
+                    console.log(nonlex);
+                    dbo.collection("nonlexWords").insertOne({
+                        nonlex: nonlex
+                    }, (err, result) => {
+                        if (err) throw err;
+                        resolve(db.db("lingDB"));
+                    })
+                }
+                resolve(db.db("lingDB"));
+            })
+        });
+    });
+}
--- index.js
+++ index.js
@@ -1,48 +1,45 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-//const db = require("./db.js");
+const db = require("./db.js");
 let fs = require('fs');
-
-//cleanup nonlex str and convert to an array
+let nonlexData;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-// const sentence = "Kim loves going to the cinema";
-// const paragraph = "Optional. The value of this provided for the call to func. Note that this may not be the actual value seen by the method."
-
-//nonlex.txt to string
-require.extensions['.txt'] = function (module, filename) {
-    module.exports = fs.readFileSync(filename, 'utf8');
-};
-
-
-const nonlexStr = require("./nonlex.txt");
-let nonlex = nonlexStr.split("\n").map(el => el.slice(0,-1));
-
-// let sent = paragraph.match(/\S.*?\."?(?=\s|$)/g).map(el=> { return getLexDensity(el) })
-// console.log(sent);
+//firts connect to db then start listening
+db.connect().then((db)=>{
+    db.collection("nonlexWords").findOne((err, item) => {
+        nonlexData = item.nonlex;
+        app.emit('ready');
+    })
+})
 
 //Use the following url query http://localhost:8080/complexity?mode=verbose&q=Some text. Any chars.
 app.get("/complexity", async (req, res) => {
     let input = req.query.q;
     let result = {};
 
-    if(req.query.mode == "verbose"){
+    if (req.query.mode == "verbose") {
         //cleaning up and converting to an array
         let sentences = input.match(/\S.*?\."?(?=\s|$)/g)
-                        .map(el=> { return getLexDensity(el) });
+            .map(el => {
+                return getLexDensity(el)
+            });
+
         //average could have been done with just calling getLexicalDensity one more time,
         //but assuming this calculation is costly and there's lots of data
-        let average = Math.round(sentences.reduce((a,b) =>
-                        { return a + b })/sentences.length*100)/100;
+        let average = Math.round(sentences.reduce((a, b) => {
+            return a + b
+        }) / sentences.length * 100) / 100;
+
         result = {
-                data:{
-                    sentence_ld: sentences,
-                    overall_ld: average
-                }
+            data: {
+                sentence_ld: sentences,
+                overall_ld: average
+            }
         }
     } else {
         result = {
@@ -58,11 +55,13 @@ app.get("/complexity", async (req, res) => {
 function getLexDensity(input){
     //make an arr and cleanup spaces
     let total = input.split(' ').filter(n => { return n != '' });
-    let lex = total.filter(val => !nonlex.includes(val));
+    let lex = total.filter(val => !nonlexData.includes(val));
 
     let lexDensity = Math.round(lex.length/total.length*100)/100;
     console.log(total, lex, "dens", lexDensity);
     return lexDensity;
 };
 
-app.listen(process.env.PORT || 8080, () => console.log("listening on 8080"));
+app.on('ready', function() {
+    app.listen(process.env.PORT || 8080, () => console.log("listening on 8080"));
+});
